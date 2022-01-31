fa79baaebd8b015cc76edd0453f9c4387b6c4f96
emanuel caro <darkemlord@gmail.com>
2022-01-31 21:39:27 +0900

    connecting mongo

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config({ path: ".env"})
 
-app.listen(3000, () => {
+mongoose.connect(process.env.MONGO_DB, () => {
+  app.listen(3000, () => {
   console.log(`server started at http://localhost:3000`)
 })
+
+});
