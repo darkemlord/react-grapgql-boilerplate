1347d1c38e9122404602108f5af46a7d9d86052e
emanuel caro <darkemlord@gmail.com>
2022-02-01 15:36:39 +0900

    apollo client connected with server

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react';
+import { ApolloProvider } from '@apollo/client';
+import client from './config/apollo'
 
 function App() {
   return (
-    <div className="App">
-      <Button primary>Primary</Button>
-      <Button secondary>Secondary</Button>
-      <h1>hello</h1>
-    </div>
+    < ApolloProvider client={client}>
+      <div className="App">
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+        <h1>hello</h1>
+      </div>
+    </ApolloProvider>
   );
 }
 
