d1fe6fb9e4a0fb0dbd9bf3d463eb956d95d518e2
emanuel caro <darkemlord@gmail.com>
2022-02-01 13:01:06 +0900

    refactoring coding, controlles folder created

---
+++ server/controllers/user.js
@@ -0,0 +1,34 @@
+const User = require('../models/user');
+const bcryptjs = require('bcryptjs');
+
+const register = async (input) => {
+  const newUser = input
+  newUser.email = newUser.email.toLowerCase();
+  newUser.username = newUser.username.toLowerCase();
+
+  const { email, username, password } = newUser;
+
+  //Check if the email is already in use
+
+  const foundEmail = await User.findOne({ email });
+  if(foundEmail) throw new Error("the email is already in use");
+
+  // Check if the username exist
+  const foundUserName = await User.findOne({ username });
+  if(foundUserName) throw new Error("The username already exist");
+
+  // Encrypt
+  const salt = await bcryptjs.genSaltSync(10);
+  newUser.password = await bcryptjs.hash(password, salt);
+
+  try{
+    const user = new User(newUser);
+    user.save();
+    return user
+
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+module.exports = { register }
--- server/gql/resolvers.js
+++ server/gql/resolvers.js
@@ -1,5 +1,5 @@
-const User = require('../models/user');
-const bcryptjs = require('bcryptjs');
+const userController = require('../controllers/user');
+
 const resolvers = {
   Query: {
     getUser: () => {
@@ -9,35 +9,7 @@ const resolvers = {
   },
   Mutation: {
     //user
-    register: async (_, { input }) => {
-      const newUser = input
-      newUser.email = newUser.email.toLowerCase();
-      newUser.username = newUser.username.toLowerCase();
-
-      const { email, username, password } = newUser;
-
-      //Check if the email is already in use
-
-      const foundEmail = await User.findOne({ email });
-      if(foundEmail) throw new Error("the email is already in use");
-
-      // Check if the username exist
-      const foundUserName = await User.findOne({ username });
-      if(foundUserName) throw new Error("The username already exist");
-
-      // Encrypt
-      const salt = await bcryptjs.genSaltSync(10);
-      newUser.password = await bcryptjs.hash(password);
-
-      try{
-        const user = new User(newUser);
-        user.save();
-        return user
-
-      } catch(err) {
-        console.log(err)
-      }
-    }
+    register: async (_, { input }) => userController.register(input)
   },
 };
 
