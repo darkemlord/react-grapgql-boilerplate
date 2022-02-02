4a97e698a5ecbccc6ddc5bd9af39271e1d36fd15
emanuel caro <darkemlord@gmail.com>
2022-02-02 10:24:37 +0900

    adding context folder and context function for Auth

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,20 @@ import { ApolloProvider } from '@apollo/client';
 import { ToastContainer } from 'react-toastify';
 import client from './config/apollo';
 import Auth from './pages/Auth';
-
+import { getToken } from './utils/token';
 
 function App() {
   const [auth, setAuth] = useState(undefined);
-  useEffect()
+
+  useEffect(() => {
+    const token = getToken();
+    if(!token){
+      setAuth(null)
+    } else {
+      setAuth(token)
+    }
+  }, []);
+
   return (
     < ApolloProvider client={client}>
        { !auth ? <Auth />: <h1>Estas logueado</h1>}
