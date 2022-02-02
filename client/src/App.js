bc95a7897471e8999b7ffafd2bebf0d146b0af2f
emanuel caro <darkemlord@gmail.com>
2022-02-02 10:06:22 +0900

    get storage item function added to token utils file

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { ToastContainer } from 'react-toastify';
 import client from './config/apollo';
 import Auth from './pages/Auth';
 
+
 function App() {
-  const [auth, setAuth] = useState(undefined)
+  const [auth, setAuth] = useState(undefined);
+  useEffect()
   return (
     < ApolloProvider client={client}>
        { !auth ? <Auth />: <h1>Estas logueado</h1>}
--- client/src/utils/token.js
+++ client/src/utils/token.js
@@ -1,5 +1,10 @@
 import { TOKEN } from './constants';
 
 export const setToken = (token) => {
-  localStorage.setItem(TOKEN, token)
+  localStorage.setItem(TOKEN, token);
+}
+
+
+export const getToken = (token) => {
+  return localStorage.getItem(TOKEN);
 }
