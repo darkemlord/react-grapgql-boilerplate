6da1771cd4858f87cdbb6157c764369cb34b0c53
emanuel caro <darkemlord@gmail.com>
2022-02-10 12:45:22 +0900

    updating home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import useAuth from '../../hooks/useAuth';
+import './Home.scss'
+
 export const Home = () => {
-  const auth = useAuth();
-  console.log(auth)
   return <div>
     <h1>estamos en la home</h1>
   </div>;
